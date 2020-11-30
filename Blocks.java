2273bbdb14bfc1e427727dca8c4bb5b61c949375
sowmya <sowmyaku.xworkz@gmail.com>
2020-11-30 19:16:10 +0530

    blocking

---
+++ Blocks.java
@@ -0,0 +1,18 @@
+class Blocks
+{
+		public static void main(String a[])
+		{
+			Blocks B=new Blocks();
+			System.out.println(B);
+		}
+		
+		{
+			System.out.println("Hi Blocks");
+		}
+		static{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+		}
+}
